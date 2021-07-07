675c7408d8a26c9dc660986f904ab032306e1b85
Saulo Mendes <saulomendes_br@hotmail.com>
2021-07-07 11:07:58 -0300

    Criação dos componentes Create/Edit/List e Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FilmesCreateComponent } from './filmes-create/filmes-create.component';
+import { FilmesEditComponent } from './filmes-edit/filmes-edit.component';
+import { FilmesListComponent } from './filmes-list/filmes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmesCreateComponent,
+    FilmesEditComponent,
+    FilmesListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/filmes-edit/filmes-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filmes-edit',
+  templateUrl: './filmes-edit.component.html',
+  styleUrls: ['./filmes-edit.component.css']
+})
+export class FilmesEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
